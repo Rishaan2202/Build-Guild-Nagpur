fa6ae75e24b2ec9f5110bd6d5495e1578af84b78
Rishaan <rishaan.sharma2202@gmail.com>
2026-03-31 13:40:34 +0530

    Added some responsiveness

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ document.addEventListener("mousemove", (e) => {
     const xAxis = document.getElementById("x-axis");
     const yAxis = document.getElementById("y-axis");    
     xAxis.style.top = `${e.clientY}px`;
-    yAxis.style.left = `${e.clientX}px`;
+    xAxis.style.left = `${e.clientX}px`;
+    yAxis.style.bottom = `${e.clientY}px`;
+    yAxis.style.right = `${e.clientX}px`;
 });
 
-const home = document.getElementById("Home");
-home.addEventListener("click", () => {
+const home = document.getElementById("home");
+home.addEventListener("onclick", homeClick() => {
+
+    home.style.backgroundColor = "#ff0000";
+    home.style.borderRadius = "5px";   
+    home.style.padding = "10px 20px";
+
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
-
-    home.style.padding = "10px 20px";
-    home.style.backgroundColor = "#ff0000";
-    home.style.borderRadius = "5px";
+    
 });
